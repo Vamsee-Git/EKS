0a5f99c3281632daef907bfe8a8970941c7e481c
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-10 17:26:54 +0530

    Update variables.tf

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -36,10 +36,14 @@ variable "repo_name" {
 
 # In the variables.tf of the EKS module
 
-variable "image_url" {
+variable "patient_image" {
   description = "The URL of the Appointment service Docker image in ECR"
   type        = string
+}
 
+variable "appointment_image" {
+  description = "The URL of the Appointment service Docker image in ECR"
+  type        = string
 }
 
 # ------------------------------
