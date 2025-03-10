943bc14a79d686c57a430f9594b4e5babf55e2fc
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-03-10 09:05:53 +0530

    Update variables.tf

--- Infra/Modules/EKS/variables.tf
+++ Infra/Modules/EKS/variables.tf
@@ -54,12 +54,6 @@ variable "max_size" {
   default     = 3
 }
 
-# In the variables.tf of the EKS module
-
-variable "image_url_patient" {
-  description = "The URL of the patient service Docker image in ECR"
-  type        = string
-}
 
 # In the variables.tf of the EKS module
 
